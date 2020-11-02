7efed355e0c506a88a9d0560413d098890813c5d
sjwan <1632516596@qq.com>
2020-11-02 16:41:54 +0800

    all

---
+++ spring-test-mgr1/src/main/java/com/example/springtestmgr1/SpringTestMgr1Application.java
@@ -0,0 +1,13 @@
+package com.example.springtestmgr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestMgr1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestMgr1Application.class, args);
+    }
+
+}
